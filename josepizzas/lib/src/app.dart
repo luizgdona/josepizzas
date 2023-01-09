3fc756585c44acb4f8ab9ce26da04997684b974c
Luiz Gustavo Doná <luizgdona@outlook.com>
2023-01-09 18:28:58 -0300

    start app josepizza

---
+++ josepizzas/lib/src/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'views/tables_page.dart';
+
+class JosePizza extends StatelessWidget {
+  const JosePizza({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
