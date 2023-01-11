87450533eedf86140d0ead3e9741e9a6675f9982
Luiz Gustavo Doná <luizgdona@outlook.com>
2023-01-11 20:49:41 -0300

    fix the status for 'mesa'

--- josepizzas/lib/src/views/tables_page.dart
+++ josepizzas/lib/src/views/tables_page.dart
@@ -4,17 +4,25 @@ import 'package:josepizzas/src/widgets/app_bar.dart';
 import '../widgets/table_list.dart';
 
 class TablePage extends StatefulWidget {
-  const TablePage({Key? key, required this.title}) : super(key: key);
-  final String title;
+  const TablePage({
+    Key? key,
+    required String title,
+  }) : super(key: key);
 
   @override
   State<TablePage> createState() => _TablePageState();
 }
 
 class _TablePageState extends State<TablePage> {
-  void _incrementCounter() {
-    setState(() {});
+  //int tappedIndex;
+
+  @override
+  void initState() {
+    super.initState();
   }
+
+  final mesas = List<bool>.generate(12, (index) => false);
+  //bool greenRed = true;
 
   @override
   Widget build(BuildContext context) {
@@ -23,13 +31,22 @@ class _TablePageState extends State<TablePage> {
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: SingleChildScrollView(
-          child: ListView.builder(
-              itemCount: 15,
-              itemBuilder: (BuildContext context, int index) {
-                return TableCard(
-                  mesaText: 'Mesa $index',
-                );
-              }),
+          child: Column(children: [
+            ListView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                itemCount: mesas.length,
+                itemBuilder: (context, index) {
+                  return TableCard(
+                      cardColor: mesas[index] ? Colors.red : Colors.green,
+                      mesaText: 'Mesa $index',
+                      tapTap: () {
+                        setState(() {
+                          mesas[index] = !mesas[index];
+                        });
+                      });
+                }),
+          ]),
         ),
       ),
     );
--- josepizzas/lib/src/widgets/table_list.dart
+++ josepizzas/lib/src/widgets/table_list.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 
 class TableCard extends StatelessWidget {
-  const TableCard({Key? key, required this.mesaText}) : super(key: key);
+  TableCard({
+    Key? key,
+    required this.mesaText,
+    required this.tapTap,
+    required this.cardColor,
+  }) : super(key: key);
 
   final String mesaText;
+  dynamic tapTap;
+  dynamic cardColor;
 
   @override
   Widget build(BuildContext context) {
     return Card(
+      color: cardColor,
       child: ListTile(
         leading: const FlutterLogo(),
         title: Text(mesaText),
+        onTap: tapTap,
       ),
     );
   }
